a46594fd4d559d217354cf7e26711343b78bcf50
Jang <gusdn6763@gmail.com>
2019-10-15 15:13:23 +0900

    2019-10-15

--- Assets/02.Scripts/Main/UIManager.cs
+++ Assets/02.Scripts/Main/UIManager.cs
@@ -1,11 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour
+{
+    public GameObject settingsPanel;
+    AudioSource audioSource;
+    Coroutine settingsCoroutine;
 
-    public void OnClickStartBtn()
+
+    private void Start()
     {
-        Debug.Log("Click Button");
+        settingsPanel.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    public void OpenSettings()
+    {
+        // 설정창 열기
+        settingsPanel.SetActive(true);
+        if (settingsCoroutine != null) StopCoroutine(settingsCoroutine);
+        settingsCoroutine = StartCoroutine(openAndCloseSettings(true, 1));
+    }
+    IEnumerator openAndCloseSettings(bool isOpen, float duration, Action callback = null)
+    {
+        CanvasGroup canvas = settingsPanel.GetComponent<CanvasGroup>();
+        float currentTime = 0;
+        while (currentTime < duration)
+        {
+            if (isOpen)
+            {
+                canvas.alpha = Mathf.Lerp(0, 1, currentTime / duration);
+            }
+            else
+            {
+                canvas.alpha = Mathf.Lerp(1, 0, currentTime / duration);
+            }
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        if (callback != null)
+            callback.Invoke();
+    }
+
+    public void CloseSettings()
+    {
+        // 설정창 닫기
+
+        if (settingsCoroutine != null) StopCoroutine(settingsCoroutine);
+        settingsCoroutine = StartCoroutine(openAndCloseSettings(false, 1, () => { settingsPanel.SetActive(false); }));
+
     }
 }
---
+++ Assets/02.Scripts/Test.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Engine
+{
+    string name;
+}
+
+interface IPart
+{
+    void TestFunc();
+}
+
+public class EngineA :Engine
+{
+    string name;
+    public EngineA()
+    {
+        name = "Ae";
+    }
+
+    public void StartEngineA()
+    {
+        //a
+    }
+
+
+}
+
+public class EngineB : Engine
+{
+    string name;
+    public EngineB()
+    {
+        name = "Be";
+    }
+
+    public void StartEngineB()
+    {
+        //a
+    }
+}
+
+public class TireA
+{
+    string name;
+    public TireA()
+    {
+        name = "At";
+    }
+}
+
+public class TireB
+{
+    string name;
+    public TireB()
+    {
+        name = "Bt";
+    }
+}
+
+
+
+public class Car
+{
+    public Engine engine;
+    public TireB tire { get; set; }
+
+    string name;
+
+    public Car()
+    {
+        engine = new EngineA();
+        engine = new EngineB();
+
+    }
+
+    public void Run()
+    {
+        //run
+    }
+}
+
+
+public class Test 
+{
+    public Test()
+    {
+        car = new Car();
+    }
+
+    private Car car;
+    public Car Car
+    {
+        get
+        {
+            return car;
+        }
+    }
+
+    void SomeMethod()
+    {
+        int? hp = 10;
+    }
+}
